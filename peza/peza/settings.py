124baf9d0518a9b3b41fdd87dad97a0aaecd2321
Bernard411 <hamiebernard@gmail.com>
2025-09-25 15:47:46 -0500

    update

--- peza/peza/settings.py
+++ peza/peza/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fa_o&+-4njxffj(ds#jcwc1-pb2-oqi90vu&08uoir64#r&ry2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
